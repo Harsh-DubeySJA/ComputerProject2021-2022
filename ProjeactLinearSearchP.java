72d1ccbb46b7956b2c24acc9941e0d04280cc834
Harsh Dubey <6350@saintjohnsacademy.com>
2021-06-23 19:25:03 +0530

    Computer Project completed

---
+++ ProjeactLinearSearchP.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ProjeactLinearSearchP {
+    public static int linearSearch(int a[], int item){
+        // return the position of 'item' in array 'a', if found else return -1
+        int foundAt=-1;
+        for(int pos=0; pos<a.length-1;pos++){
+            if(a[pos]==item){
+                foundAt=pos;
+                break;
+            }
+        }
+        return foundAt;//not found
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements :: ");
+        int array[] = new int[sc.nextInt()];
+        System.out.println("Enter elements :: ");
+        for (int i = 0; i < array.length; i++)
+            array [i] = sc.nextInt();
+        System.out.print("Enter the element to be serached for :: ");
+        int item = sc.nextInt();
+        sc.close();
+        System.out.println("Element present at " + linearSearch(array,item) + " position");
+        System.out.println(java.util.Arrays.toString(array));
+    }
+}
